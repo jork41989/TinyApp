c0b04337ff05c89381444f8cc7d2a43a1908b9aa
jork41989 <jork41989@gmail.com>
2021-04-08 14:47:33 -0400

    added model for question

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> console.log('mongo is a gogo'))
+  .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => res.send("HEY YOU MADE IT!"))
 
